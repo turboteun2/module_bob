a76085ee18f6772eaa0c7af96397d029dedc3a42
Teun Grondman <67547252+turboteun2@users.noreply.github.com>
2025-01-10 16:05:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
+from speech import activate_word
+import voice_talk
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*") 
 
 received_text = "" 
+# Server is combining the two snippets from speech.py and voice_talk.py
+# Python is only sending data to the mirror, so it's showing what is being said.
+# TODO: Implement text from mirror.
 
-@app.route('/send', methods=['POST'])
+@app.route('/add', methods=['POST']) # For testing
 def send():
     global received_text
     data = request.json
     received_text = data.get("text", "")
     
     socketio.emit('update_text', {"text": received_text})
-    
-    return jsonify({"message": "Text received!"})
+    voice_talk.generate_voice(received_text)
+    return jsonify({"text": "Text received!"})
 
-@app.route('/receive', methods=['GET'])
-def receive():
-    return jsonify({"text": received_text})
+@app.route('/sendData', methods=['POST']) # For testing
+def send_data():
+    
+    answer_llama = "I am a llama, I don't know"
+    voice_talk.generate_voice(answer_llama)
+    return jsonify({"text": f"{answer_llama}"})
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5001)
+    activate_word()	# start the speech recognition
