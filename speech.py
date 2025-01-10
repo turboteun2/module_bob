c39fcad41160a9f107dbd5524799cf9fa9c172a5
Teun Grondman <67547252+turboteun2@users.noreply.github.com>
2025-01-10 16:02:31 +0100

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,46 +1,30 @@
-import torch
-from torch.nn.attention import SDPBackend, sdpa_kernel
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
-from tqdm import tqdm
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO
+import voice_talk
 
-torch.set_float32_matmul_precision("high")
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*") 
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+received_text = "" 
+# Server is combining the two snippets from speech.py and voice_talk.py
+# Python is only sending data to the mirror, so it's showing what is being said.
+# TODO: Implement text from mirror.
 
-model_id = "openai/whisper-large-v3"
+@app.route('/add', methods=['POST']) # For testing
+def send():
+    global received_text
+    data = request.json
+    received_text = data.get("text", "")
+    
+    socketio.emit('update_text', {"text": received_text})
+    voice_talk.generate_voice(received_text)
+    return jsonify({"text": "Text received!"})
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True
-).to(device)
+@app.route('/sendData', methods=['POST']) # For testing
+def send_data():
+    
+    answer_llama = "I am a llama, I don't know"
+    return jsonify({"text": f"{answer_llama}"})
 
-# Enable static cache and compile the forward pass
-model.generation_config.cache_implementation = "static"
-model.generation_config.max_new_tokens = 256
-model.forward = torch.compile(model.forward, mode="reduce-overhead", fullgraph=True)
-
-processor = AutoProcessor.from_pretrained(model_id)
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device,
-)
-
-dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
-sample = dataset[0]["audio"]
-
-# 2 warmup steps
-for _ in tqdm(range(2), desc="Warm-up step"):
-    with sdpa_kernel(SDPBackend.MATH):
-        result = pipe(sample.copy(), generate_kwargs={"min_new_tokens": 256, "max_new_tokens": 256})
-
-# fast run
-with sdpa_kernel(SDPBackend.MATH):
-    result = pipe(sample.copy())
-
-print(result["text"])
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5001)
